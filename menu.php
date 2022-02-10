f1bfd485f52f68f3d2761c5602c99f5d11e4492f
PChl0e <97352210+PChl0e@users.noreply.github.com>
2022-02-10 08:14:46 +0100

    début

---
+++ menu.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="menu.css">
+</head>
+<body>
+<div class="menu-nav">
+        <div class="logo">
+            <img src="" alt="logo">
+        </div>
+        <nav class="nav" id="nav-menu">
+            <ion-icon name="close-outline" class="header_close" id="close-menu"></ion-icon>
+            <ul class="nav_list">
+                <li class="nav_items"><a href="#" class="nav_links">Accueil</a></li>
+                <li class="nav_items"><a href="#" class="nav_links">A propos de moi</a></li>
+                <li class="nav_items"><a href="#" class="nav_links">Portfolio</a></li>
+                <li class="nav_items"><a href="#" class="nav_links">Contact</a></li>
+            </ul>
+        </nav>
+        <ion-icon name="menu-outline" class="header_toggle" id="toggle-menu"></ion-icon>
+    </div>
+</body>
+</html>
